ec72ef8a29219b9ce56da76a0197b0a5fde6a960
brianartea <bri@expertdesign.cc>
2021-07-07 09:19:34 -0400

    tried to add another animation keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,25 @@ module.exports = {
           "100%": {
             transform: "translate(0px, 0px) scale(1)",
           },
+          animation: {
+            blobtwo: "blobtwo 7s infinite",
+          },
+          keyframes: {
+            blobtwo: {
+              "0%": {
+                transform: "translate(0px, 0px) scale(1)",
+              },
+              "33%": {
+                transform: "translate(30px, -50px) scale(1.1)",
+              },
+              "66%": {
+                transform: "translate(-20px, 20px) scale(0.9)",
+              },
+              "100%": {
+                transform: "translate(0px, 0px) scale(1)",
+              },
+            },
+          },
         },
       },
     },
