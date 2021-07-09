072c1bd4bf5b625c25b00e8924be7869d5bdad0b
brianartea <bri@expertdesign.cc>
2021-07-09 09:39:45 -0400

    commit

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
           <div className="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70 animate-blob"></div>
           <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70 animate-blob animation-delay-2000"></div>
           <div className="absolute -bottom-8 left-20  w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70 animate-blob animation-delay-4000"></div>
-          <div className="m-8 relative space-y-4 opacity-1">
+          <div className="m-8 relative space-y-4 opacity-0">
             <div className="p-5 bg-white rounded-lg flex items-center justify-between space-x-8">
               <div className="flex-1">
                 <div className="h-4 w-48 bg-gray-300 rounded"></div>
